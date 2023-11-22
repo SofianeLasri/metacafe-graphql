cdf2943b09e0afa0c47e0b41ae0a3e6b8bc088a8
SofianeLasri <alasri250@gmail.com>
2023-11-22 18:26:57 +0100

    Création du routeur

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './scss/app.scss'
 import App from './App.vue'
+import router from "./router.ts";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router); // Utilisez le routeur
+
+app.mount('#app');
