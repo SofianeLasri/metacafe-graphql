c343504ed10e349a9e81721f9ec58d9b07e3781e
Sofiane Lasri-Trienpont <alasri250@gmail.com>
2023-12-09 12:49:04 +0100

    Modification du middleware isAuthenticated pour supporter le token bearer

--- server/src/api/infrastructure/authentication.ts
+++ server/src/api/infrastructure/authentication.ts
@@ -7,10 +7,21 @@ import {ExpressApplication} from "./express-application";
 export const jsonParser = bodyParser.json();
 
 export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
-    if (req.isAuthenticated()) {
-        return next();
+    const token: string | undefined = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({message: 'Non authentifié'});
     }
-    res.status(401).json({message: 'Non authentifié'});
+
+    const jwtToken = token.replace("Bearer ", "");
+
+    jwt.verify(jwtToken, ExpressApplication.getSessionSecret(), (err, verified) => {
+        if (err) {
+            return res.status(401).json({message: 'Non authentifié'});
+        }
+        req.user = verified;
+        next();
+    });
 }
 
 export const generateAuthToken = (user: User) => {
